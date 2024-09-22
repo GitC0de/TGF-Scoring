588345c71c07350ee9a438d1ccf7a92235575171
GitC0de <joonyihn@gmail.com>
2024-09-22 19:59:47 +0900

    input division

--- src/System.js
+++ src/System.js
@@ -1,5 +1,5 @@
-import { React, useState } from "react";
-/* import "./System.css"; */
+import { React, useState, useRef, useEffect } from "react";
+import "./System.css";
 export default function System() {
   const rightAnswer = [
     5, 5, 1, 5, 3, 2, 4, 4, 2, 1, 5, 3, 4, 2, 5, 4, 3, 4, 1, 3,
@@ -12,12 +12,39 @@ export default function System() {
   const wrongProblems = [];
 
   let score = 0;
+  let answer = "";
 
-  const [answer, setAnswer] = useState("");
+  const [answer1, setAnswer1] = useState("");
+  const [answer2, setAnswer2] = useState("");
+  const [answer3, setAnswer3] = useState("");
+  const [answer4, setAnswer4] = useState("");
   const [totalScore, setTotalScore] = useState(0);
   const [wrong, setWrong] = useState([]);
 
+  const firstRef = useRef("");
+  const secondRef = useRef("");
+  const thirdRef = useRef("");
+  const fourthRef = useRef("");
+
+  useEffect(() => {
+    firstRef.current.focus();
+  }, []);
+
+  useEffect(() => {
+    answer1.length >= 5 && secondRef.current.focus();
+  }, [answer1]);
+
+  useEffect(() => {
+    answer2.length >= 5 && thirdRef.current.focus();
+  }, [answer2]);
+
+  useEffect(() => {
+    answer3.length >= 5 && fourthRef.current.focus();
+  }, [answer3]);
+
   const scoring = () => {
+    answer = answer1 + answer2 + answer3 + answer4;
+
     if (answer.length !== 20) {
       alert("답 개수가 " + answer.length + "개입니다. 다시 입력해주세요.");
     } else {
@@ -38,18 +65,46 @@ export default function System() {
   };
 
   const clear = () => {
-    setAnswer("");
+    setAnswer1("");
+    setAnswer2("");
+    setAnswer3("");
+    setAnswer4("");
     setTotalScore(0);
     setWrong([]);
   };
 
   return (
     <>
-      <input
-        placeholder="답을 입력하세요!"
-        value={answer}
-        onChange={(e) => setAnswer(e.target.value)}
-      ></input>
+      <div>
+        <input
+          placeholder="1~5 답 입력"
+          value={answer1}
+          onChange={(e) => setAnswer1(e.target.value)}
+          className="answerInput"
+          ref={firstRef}
+        ></input>
+        <input
+          placeholder="6~10 답 입력"
+          value={answer2}
+          onChange={(e) => setAnswer2(e.target.value)}
+          className="answerInput"
+          ref={secondRef}
+        ></input>
+        <input
+          placeholder="11~15 답 입력"
+          value={answer3}
+          onChange={(e) => setAnswer3(e.target.value)}
+          className="answerInput"
+          ref={thirdRef}
+        ></input>
+        <input
+          placeholder="16~20 답 입력"
+          value={answer4}
+          onChange={(e) => setAnswer4(e.target.value)}
+          className="answerInput"
+          ref={fourthRef}
+        ></input>
+      </div>
       <button onClick={scoring} className="scoringButton">
         채점
       </button>
