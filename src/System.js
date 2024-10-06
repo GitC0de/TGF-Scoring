06d87b1916bb82f3754f64c3e976cc85ce2b5733
GitC0de <joonyihn@gmail.com>
2024-10-06 12:59:27 +0900

    13th week update

--- src/System.js
+++ src/System.js
@@ -2,11 +2,11 @@ import { React, useState, useRef, useEffect } from "react";
 import "./System.css";
 export default function System() {
   const rightAnswer = [
-    5, 1, 2, 3, 1, 2, 4, 1, 3, 3, 2, 3, 1, 5, 5, 2, 5, 3, 4, 4,
+    5, 5, 1, 1, 2, 4, 1, 3, 1, 5, 3, 5, 4, 3, 5, 4, 2, 3, 3, 2,
   ];
 
   const questionScore = [
-    2, 2, 3, 2, 2, 2, 2, 2, 3, 3, 3, 2, 2, 3, 3, 3, 3, 2, 3, 3,
+    2, 2, 3, 3, 2, 2, 3, 2, 2, 3, 3, 3, 2, 3, 3, 2, 3, 2, 3, 2,
   ];
 
   const wrongProblems = [];
