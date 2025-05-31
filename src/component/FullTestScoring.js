bf8aef89ec0ac84deae1069ae8df4ccab5246b4f
GitC0de <joonyihn@gmail.com>
2025-05-31 15:26:34 +0900

    5th week update

--- src/component/FullTestScoring.js
+++ src/component/FullTestScoring.js
@@ -5,11 +5,11 @@ import "./FullTestScoring.css";
 export default function FullTestScoring() {
   const nav = useNavigate();
   const rightAnswer = [
-    1, 3, 5, 2, 3, 4, 5, 4, 1, 2, 2, 3, 4, 1, 1, 2, 3, 4, 5, 5,
+    5, 3, 5, 4, 5, 1, 1, 4, 1, 4, 2, 2, 4, 1, 5, 5, 2, 3, 3, 1,
   ];
 
   const questionScore = [
-    2, 2, 2, 2, 2, 3, 3, 3, 2, 2, 3, 3, 3, 2, 3, 3, 3, 2, 3, 2,
+    2, 2, 2, 2, 2, 3, 3, 3, 3, 2, 3, 2, 2, 3, 3, 3, 3, 2, 3, 2,
   ];
 
   const wrongProblems = [];
@@ -106,7 +106,7 @@ export default function FullTestScoring() {
 
   return (
     <>
-      <h1>2026 AS 3주차 채점(Beta)</h1>
+      <h1>2026 AS 5주차 채점(Beta)</h1>
       <p className="confirmation">
         ※ 주차 & 답안을 확인해주세요! 저번 주차로 되어 있거나 정답이 맞지
         않으면, 업데이트가 되지 않은 것입니다!
